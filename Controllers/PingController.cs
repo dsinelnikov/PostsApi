8209f2fde07f6bbae8c1d91d41ea4a0a9b861885
LOHIKA\dsinelnikov <dsinelnikov@lohika.com>
2018-04-20 13:35:50 +0300

    Add Ping controller

---
+++ Controllers/PingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PostsApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Ping")]
+    public class PingController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Alive";
+        }
+    }
+}
